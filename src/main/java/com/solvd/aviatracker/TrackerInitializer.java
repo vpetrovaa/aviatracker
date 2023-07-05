06f7135a4e2a4370c57ca6695ce948a6b587e7d8
vpetrovaa <vpetrova.laba@solvd.com>
2023-07-05 18:06:11 +0300

    Project structure was generated

---
+++ src/main/java/com/solvd/aviatracker/TrackerInitializer.java
@@ -0,0 +1,13 @@
+package com.solvd.aviatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerInitializer.class, args);
+	}
+
+}
