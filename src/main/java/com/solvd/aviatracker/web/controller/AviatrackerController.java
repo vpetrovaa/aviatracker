327c9d062673edf85b3aadacf6bd153ab7988c68
vpetrovaa <vpetrova.laba@solvd.com>
2023-07-11 16:15:46 +0300

    Controller was renamed

--- src/main/java/com/solvd/aviatracker/web/controller/AviatrackerController.java
+++ src/main/java/com/solvd/aviatracker/web/controller/AviatrackerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/aviatracker")
 @RequiredArgsConstructor
-public final class AviaTrackerController {
+public final class AviatrackerController {
 
     private final AviatrackerService aviatrackerService;
 
